f1e711376d10e4a0571f7f9ecc5f13f9411422d3
Jai Lakhanpal <jlak212@gmail.com>
2020-09-05 16:51:30 -0400

    added logging

--- context_managers/open_file.py
+++ context_managers/open_file.py
@@ -2,12 +2,21 @@
 
 import argparse
 from contextlib import contextmanager
+import logging
 
 
 def cli():
     parser = argparse.ArgumentParser()
     parser.add_argument("input", help="file to write")
-    return parser.parse_args()
+    parser.add_argument(
+        "-q", "--quiet", help="only show warnings and errors", action="store_true"
+    )
+    args = parser.parse_args()
+
+    format = "%(levelname)-5s | %(message)s"
+    level = logging.WARNING if args.quiet else logging.INFO
+    logging.basicConfig(format=format, level=level)
+    return args
 
 
 class Open_File:
@@ -16,37 +25,38 @@ class Open_File:
         self.mode = mode
 
     def __enter__(self):
-        print("Open_File.__enter__(): opening file...")
+        logging.info("Open_File.__enter__(): opening file...")
         self.file = open(self.filename, self.mode)
         return self.file
 
     def __exit__(self, exc_type, exc_val, traceback):
-        print("Open_File.__exit__(): closing file...")
+        logging.info("Open_File.__exit__(): closing file...")
         self.file.close()
 
 
 @contextmanager
 def open_file(file, mode):
     try:
-        print("open_file(): opening file...")
+        logging.info("open_file(): opening file...")
         f = open(file, mode)
         yield f
     finally:
-        print("open_file(): closing file...")
+        logging.info("open_file(): closing file...")
         f.close()
 
 
 def main():
     args = cli()
-    print("\nClass implementation of context manager")
+
+    # Class implementation of context manager
     with Open_File(args.input, "w") as f:
-        print(f"Writing to {args.input}...")
+        logging.info(f"Writing to {args.input}...")
         f.write("Testing")
     print(f"{args.input} closed?", f.closed)
 
-    print("\nFunction implementation of context manager")
+    # Function implementation of context manager
     with open_file(args.input, "w") as f:
-        print(f"Writing to {args.input}...")
+        logging.info(f"Writing to {args.input}...")
         f.write("Testing")
     print(f"{args.input} closed?", f.closed)
 
